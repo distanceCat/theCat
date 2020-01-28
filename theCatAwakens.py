438b6082a68d66c55816397aa1f0f4e6b9a7d713
distanceCat <norman.ihle@web.de>
2020-01-28 12:21:06 +0100

    added a second cat

--- theCatAwakens.py
+++ theCatAwakens.py
@@ -4,8 +4,15 @@ import cat
 if __name__ == '__main__':
     myCat = cat.cat()
     myCat.setName("Moritz")
-    myCat.setColor("black")
-    myCat.setAge(5)
+    myCat.setColor("white-brown")
+    myCat.setAge(2)
     print(myCat.getCatDescription())
     myCat.addSound("Mauz")
     print(myCat.makeSound())
+
+    mySecondCat = cat.cat()
+    mySecondCat.setName("James")
+    mySecondCat.setColor("black")
+    mySecondCat.setAge(5)
+    print(mySecondCat.getCatDescription())
+    print(mySecondCat.makeSound())
